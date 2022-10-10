d9fea134f75c68908c3e2d50d54aeec6691988f4
marloBarksdale <lyndenflood@gmail.com>
2022-10-10 20:24:45 +0000

    hello world

--- tinkering.js
+++ tinkering.js
@@ -3,3 +3,22 @@ const multiply = (a, b) => a * b;
 const result = multiply(2, 4);
 
 console.log(result);
+
+var richard = {
+  '1stSon': true,
+  'loves-snow': true,
+};
+
+console.log(richard['1stSon']); // error
+console.log(richard['loves-snow']); // error
+
+const ob = {
+  name: 'pol',
+  changeName: function () {
+    this.name = 'cat';
+  },
+};
+
+console.log(ob);
+ob.changeName();
+console.log(ob);
